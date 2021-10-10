204bc1d3b077f54e92b4c4f3acf177264c1c5f1e
Zelechos <zelechos@hotmail.com>
2021-10-10 04:04:19 -0400

    v0.0.4

--- Js/Main.js
+++ Js/Main.js
@@ -1,29 +1,47 @@
-'use strict'
-window.addEventListener("load" , ()=>{
-    const Container = document.querySelector(".head");
-    for(let i = 0; i<= 25;i++){
-        const blocks = document.createElement('div');
-        blocks.classList.add("block");
-        Container.appendChild(blocks);
-    }
+    'use strict'
+    window.addEventListener("load" , ()=>{
 
-    function animate(){
-        anime({
-            targets : '.block',
-            translateX : function(){
-            return anime.random(0,1700)
-            },
-            translateY : function(){
-            return anime.random(-1000,1500)
-            },
-            scale : function(){
-                return anime.random(1,5)
-            },
-            easing: 'linear',
-            duration: 5000,
-            delay: anime.stagger(1),
-            complete: animate,
-        });
-    }
-    animate();
-});
+// Efocando el Input
+        const form = document.querySelector("#form");
+        form.focus();
+
+
+// Animacion de Cuadros como Background
+        const Container = document.querySelector(".head");
+        for(let i = 0; i<= 25;i++){
+            const blocks = document.createElement('div');
+            blocks.classList.add("block");
+            const textLabel = document.createElement('p');
+            textLabel.textContent = "My Pragmatic Code";
+            blocks.appendChild(textLabel);
+            Container.appendChild(blocks);
+        }
+    
+        function animate(){
+            anime({
+                targets : '.block',
+                translateX : function(){
+                return anime.random(0,1700)
+                },
+                translateY : function(){
+                return anime.random(-1000,1500)
+                },
+                scale : function(){
+                    return anime.random(1,5)
+                },
+                easing: 'linear',
+                duration: 5000,
+                delay: anime.stagger(1),
+                complete: animate,
+            });
+        }
+        animate();
+
+
+
+
+
+    });
+
+
+
---
+++ Js/dinamictext.js
@@ -0,0 +1,84 @@
+/*
+ * @Author : Pragmatic Coder
+ * @Repositorio : https://github.com/Zelechos/DynamicText
+ */
+
+'use strict'
+window.addEventListener('load',()=>{
+    
+    // let Text = document.querySelector("#DynamicText").innerHTML;
+    let Text ="< Codigo >"
+    const Vacio = document.querySelector("#DynamicText").innerHTML = " ";
+
+    //console.error(Text);
+    const array = ['_' , '|' , '_', '|' ,'_' , '|' ,'_' , '|' ] ;
+
+    const palabra = Text.split("");
+    //console.warn(palabra);
+
+    let actual = "";
+
+    for(let letra in palabra){
+        let antecesor = palabra[letra];
+        actual += antecesor;
+        //console.log(actual);
+
+        if(palabra.length % 2 == 0){
+            if(letra % 2 == 0){
+                array.push(actual+'|');
+            }else{
+                array.push(actual+'');
+            }
+        }else{
+            if(letra % 2 == 0){
+                array.push(actual+'');
+            }else{
+                array.push(actual+'|');
+            }
+        }
+            
+
+        let i=0;
+        const last = array.length-1;
+        if(palabra.length-1 == letra){
+            while(i<15){
+                if(i % 2 == 0){
+                    array.push(array[last]+'|');
+                }else{
+                    array.push(array[last]+' ');
+                }
+                i++;
+            }
+        }
+    }
+
+
+    //Introduciendo los string a las etiquetas span y las etiquetas span al array list
+    let Etiqueta = document.querySelector('#DynamicText');
+    const list = [];
+    array.forEach(element=>{
+        let letra = document.createElement('span');
+        let text = document.createTextNode(element);
+
+        letra.appendChild(text);
+        letra.style.display = "none";
+        list.push(letra);
+    });
+    //console.log(list)
+
+    //Obtenemos la Etiqueta donde insertaremos el texto dinamico
+    list.forEach(element=>{
+        Etiqueta.appendChild(element);
+    });
+
+    //Mostrarmos dinamicamente la palabra
+    let x=0;
+    function Rotacion(){
+        list[x].style.display = "none";
+        x = (x+1)%list.length;
+        list[x].style.display = "initial";
+    }
+
+    setInterval(Rotacion, 120);
+
+});
